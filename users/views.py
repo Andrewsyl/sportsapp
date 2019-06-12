76e0ad75780315ed9d6f7b00121477d62218b914
Costello <andrewsylcostello@gmail.com>
2019-06-12 15:30:06 +0100

    static files setup

--- users/views.py
+++ users/views.py
@@ -19,6 +19,8 @@ def users(request):
 
 
 def login(request):
+    if request.user.is_active:
+        return redirect('/')
     form = Login(request.POST or None)
     next = request.GET.get('next')
     if form.is_valid():
