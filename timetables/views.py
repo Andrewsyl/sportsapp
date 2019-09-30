e2bffe1e7515803b02e3925f27254fe7470d2d28
Costello <andrewsylcostello@gmail.com>
2019-09-30 16:15:17 +0100

    update

--- timetables/views.py
+++ timetables/views.py
@@ -35,20 +35,45 @@ def create_timetable_days(request):
 def create_timetable_times(request):
     club = request.user.club
     days = Day.objects.filter(club=club)
-    Periods.objects.all().delete()
-    PeriodFormSet = modelformset_factory(Periods, form=PeriodCreateForm, extra=0 if len(Periods.objects.all()) > 0 else len(Day.objects.all()))
-    form = PeriodFormSet(request.POST or None)
-    if form.is_valid():
-        for k in form:
-            start_time = k.cleaned_data['start_time']
-            end_time = k.cleaned_data['end_time']
-            period = Periods(start_time=start_time, end_time=end_time)
-            period.save()
+    pps = Periods.objects.all()
+    # PeriodFormSet = modelformset_factory(Periods, form=PeriodCreateForm, extra=0 if len(Periods.objects.all()) > 0 else len(Day.objects.all()))
+    forms = [PeriodCreateForm(request.POST, prefix=str(day.name), instance=Periods()) for day in Day.objects.all()]
 
-        return redirect('/')
+    # form = PeriodFormSet(request.POST or None)
+    if request.POST:
+        if [cf.is_valid() for cf in forms]:
+            for k in enumerate(forms):
+                start_time = k['start_time'].data
+                end_time = k['end_time'].data
+                period = Periods(start_time=start_time, end_time=end_time, day=k)
+                period.save()
+
+            return redirect('/')
 
     context = {
-        'form': form,
+        'forms': forms,
         'days': days,
     }
     return render(request, 'timetables/create_timetable_times.html', context)
+
+# @login_required(login_url='/login/')
+# def create_timetable_times(request):
+#     club = request.user.club
+#     days = Day.objects.filter(club=club)
+#     Periods.objects.all().delete()
+#     PeriodFormSet = modelformset_factory(Periods, form=PeriodCreateForm, extra=0 if len(Periods.objects.all()) > 0 else len(Day.objects.all()))
+#     form = PeriodFormSet(request.POST or None)
+#     if form.is_valid():
+#         for k in form:
+#             start_time = k.cleaned_data['start_time']
+#             end_time = k.cleaned_data['end_time']
+#             period = Periods(start_time=start_time, end_time=end_time)
+#             period.save()
+#
+#         return redirect('/')
+#
+#     context = {
+#         'form': form,
+#         'days': days,
+#     }
+#     return render(request, 'timetables/create_timetable_times.html', context)
